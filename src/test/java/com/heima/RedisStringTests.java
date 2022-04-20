aeeb7d316d1e110f6dbf9a7927b2cd15e76907cf
xfx <xfx@atguihu.com>
2022-04-20 08:46:51 +0800

    first commit

---
+++ src/test/java/com/heima/RedisStringTests.java
@@ -0,0 +1,50 @@
+package com.heima;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.heima.redis.config.pojo.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.Map;
+
+@SpringBootTest
+class RedisStringTests {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    void contextLoads() {
+        stringRedisTemplate.opsForValue().set("name","虎哥");
+        Object name = stringRedisTemplate.opsForValue().get("name");
+        System.out.println("name="+name);
+    }
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+    @Test
+    void testSaveUser() throws JsonProcessingException {
+        //创建对象
+        User user = new User("虎哥",21);
+        //手动序列化
+        String json = mapper.writeValueAsString(user);
+        //写入数据
+        stringRedisTemplate.opsForValue().set("user:200",json);
+        //获取数据
+        String jsonUser = stringRedisTemplate.opsForValue().get("user:200");
+        //手动反序列化
+        User user1 = mapper.readValue(jsonUser, User.class);
+        System.out.println("user1="+user1);
+    }
+
+    @Test
+    void testHash(){
+        stringRedisTemplate.opsForHash().put("user:400","name","邪剑仙");
+        stringRedisTemplate.opsForHash().put("user:400","age","22");
+        Map<Object, Object> entries = stringRedisTemplate.opsForHash().entries("user:400");
+        System.out.println(entries);
+    }
+
+}
